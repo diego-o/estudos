b8358c57ff18708c96bb7ba0d02b7375456aa73f
diego-o <diego_1souza@hotmail.com>
2018-07-30 18:33:25 -0300

    criando servidor node

--- NodeJS/ApiLoja/src/app.js
+++ NodeJS/ApiLoja/src/app.js
@@ -1,1 +1,25 @@
 'use strict';
+
+'use strict'
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+
+//conexão com banco de dados
+//mongoose.connect(config.connectionString);
+
+//carregando os models
+
+//carregando as rotas
+const indexRoute = require('./routes/index-route');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+//adicionar controllers
+app.use('/', indexRoute);
+
+module.exports = app;
