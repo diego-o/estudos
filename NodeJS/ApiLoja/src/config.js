f208914f2753d767afd55beae5b4742b0b6b7fa2
Diego Souza <diego_1souza@hotmail.com>
2018-08-13 17:17:50 -0300

    Update config.js

--- NodeJS/ApiLoja/src/config.js
+++ NodeJS/ApiLoja/src/config.js
@@ -1,5 +1,5 @@
 global.PRIVATE_KEY = 'c9b18230-1823-s81n-20b8-84n02n4926d8';
 
 module.exports = {
-    connectionString: 'mongodb://diego:diego123@ds018558.mlab.com:18558/api-loja'
+    connectionString: 'mongodb://user:pass@ds018558.mlab.com:18558/api-loja'
 }
